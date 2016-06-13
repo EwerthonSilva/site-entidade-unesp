0e97d5edfdc8e93e864b9c88f51f50218e03eaad
José Eduardo <damiansb@gmail.com>
2016-06-13 15:00:21 -0300

    correção do script permissions

--- admin/permissions.sh
+++ admin/permissions.sh
@@ -1,8 +1,11 @@
 chown www-data lib/
 chown www-data dbo/
+chown www-data dbo/upload/images/
+chown www-data dbo/upload/files/
 chown www-data scss/
 chown www-data scss/foundation/
 chown www-data dbo/plugins/jcrop_dbo/
+chown www-data dbo/plugins/jcrop_dbo/temp/
 
 echo " "
 echo "------------------------------------------------------"
