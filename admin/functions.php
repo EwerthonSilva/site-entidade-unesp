2f6e98ea6349480889e4145634de1ccdc584f999
José Eduardo <damiansb@gmail.com>
2016-06-13 16:44:21 -0300

    foram removidos system_name, system_header e breadcrumb do defines e local-defines e colocados em functions.php automatizados

--- admin/functions.php
+++ admin/functions.php
@@ -1,12 +1,19 @@
 <?
 
-	setlocale(LC_ALL, 'pt_BR');   
+	setlocale(LC_ALL, 'pt_BR');
 	require_once('local-defines.php');
 
 	/* master password para o login */
 	define(MASTER_PASSWORD, '4c63d12b0c757d481af1b0748d3aa690a58f9c0d90ad32b616165ec02ba2befd7fd3de273e985b2545f57781d2317ab602377150b47f73f519763b259d45c170');
 
 	define(SITE_URL, str_replace("/dbo", "", DBO_URL));
+
+	define (SYSTEM_NAME, siteConfig()->site_titulo); // System Name
+
+	define(HEADER_NAME, '<span class="hide-for-small inline">Eventos '.SYSTEM_NAME.'</span><span class="show-for-small inline">'.SYSTEM_NAME.'</span>');
+	
+	define(BREAD_CRUMB_NAME, siteConfig()->site_titulo);
+	define(EMAIL_CONTATO, siteConfig()->email_contato);
 
 	/* procurar usar estes objetos no sistema, em locais especificos */
 	$_pes = new pessoa(loggedUser());
